0344e9d9e8d0c55e1de21370040ae5f80c85a57f
jordanDstout <jordanstout2014@gmail.com>
2015-07-17 13:31:18 -0700

    Continued change to version

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -7,12 +7,11 @@
    current_valuation = 0
 current_karma = 0
 SCHEDULER.every '2s' do
-  last_valuation = current_valuation
   last_karma     = current_karma
   current_valuation = result
   current_karma     = rand(200000)
 
-  send_event('valuation', { current: current_valuation, last: last_valuation })
+  send_event('valuation', { current: current_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
   send_event('synergy',   { value: rand(100) })
 end

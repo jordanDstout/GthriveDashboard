f1b487aea00f46dc5f546719a0a5d047108fcce9
jordanDstout <jordanstout2014@gmail.com>
2015-07-17 13:24:39 -0700

    adding job

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,10 +1,15 @@
-current_valuation = 0
+ require 'net/http'
+   source = "https://secure.gthrive.com/api/client/v2/gserver_version.json?token=''"
+   resp = Net::HTTP.get_response(URI.parse(source))
+   data = resp.body
+   result=JSON.parse(data)
+   result=result["data"]["version"]
+   current_valuation = 0
 current_karma = 0
-
 SCHEDULER.every '2s' do
   last_valuation = current_valuation
   last_karma     = current_karma
-  current_valuation = rand(100)
+  current_valuation = result
   current_karma     = rand(200000)
 
   send_event('valuation', { current: current_valuation, last: last_valuation })

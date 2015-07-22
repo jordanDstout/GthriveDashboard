22e9911191011bc70eef988b14f1a43e335f1037
jordanDstout <jordanstout2014@gmail.com>
2015-07-22 14:44:06 -0700

    added all to scheduler

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,7 +1,9 @@
 require 'net/http'
 require 'time'
 
-    nodes = {}
+
+SCHEDULER.every '2s' do
+      nodes = {}
 list_of_glocations=['54']
   for x in list_of_glocations do
     token_source = "https://secure.gthrive.com/api/client/v2/gnodes.json?glocation_id="+x+"&token=2uaHJ8UzbxxFhzr5w9s5&last_configured_at=2015-07-20T16%3A37%3A30.176Z"
@@ -28,7 +30,24 @@ farm_counts["Farm Name"] = {label:"Farm Name", value: thing}
     send_event('buzzwords', { items: farm_counts.values })
 nodes.delete("name")
   
-SCHEDULER.every '2s' do
+    nodes = {}
+list_of_glocations=['54']
+  for x in list_of_glocations do
+    token_source = "https://secure.gthrive.com/api/client/v2/gnodes.json?glocation_id="+x+"&token=2uaHJ8UzbxxFhzr5w9s5&last_configured_at=2015-07-20T16%3A37%3A30.176Z"
+    resp = Net::HTTP.get_response(URI.parse(token_source))
+    data = resp.body
+    result=JSON.parse(data)
+    for name in result['configuration']['placements'] do
+      thing=name[1]
+      display_name=thing['display_name']
+      if thing["gproduct_type"]=="Glink"
+        nodes[display_name]=thing["last_heartbeat"]
+      else 
+        nodes[display_name]=thing["last_reported_at"]
+      end
+    end
+    nodes["name"]=result["configuration"]["glocations"][0]["name"]
+  end
   for key in nodes do
     key[1]=Time.parse(key[1])
     farm_counts[key[0]] = {label:key[0], value: key[1]}

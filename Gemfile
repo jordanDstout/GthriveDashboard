0b4cb42917534fff8ecfb71b348145f03ec37824
jordanDstout <jordanstout2014@gmail.com>
2015-07-22 14:39:12 -0700

    changes to Buzzwords

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ ruby '2.2.1'
 
 gem 'dashing'
 
+gem 'pry'
+
 ## Remove this if you don't need a twitter widget.
 gem 'twitter', '>= 5.9.0'
--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,7 +1,8 @@
 require 'net/http'
+require 'time'
 
     nodes = {}
-list_of_glocations=['54','212','204']
+list_of_glocations=['54']
   for x in list_of_glocations do
     token_source = "https://secure.gthrive.com/api/client/v2/gnodes.json?glocation_id="+x+"&token=2uaHJ8UzbxxFhzr5w9s5&last_configured_at=2015-07-20T16%3A37%3A30.176Z"
     resp = Net::HTTP.get_response(URI.parse(token_source))
@@ -16,17 +17,44 @@ list_of_glocations=['54','212','204']
         nodes[display_name]=thing["last_reported_at"]
       end
     end
+    nodes["name"]=result["configuration"]["glocations"][0]["name"]
   end
   
+
+
+farm_counts={}
+thing=nodes["name"]
+farm_counts["Farm Name"] = {label:"Farm Name", value: thing}
+    send_event('buzzwords', { items: farm_counts.values })
+nodes.delete("name")
   
-  
-  
-SCHEDULER.every '2s' do
-  #for key in nodes do
-  #  print (key[0])
-  #  print (key[1])
-  first= nodes.first
-    glove={ label: first[0], value: first[1] }
-    send_event('buzzwords', { items:glove.values })
+#SCHEDULER.every '2s' do
+  for key in nodes do
+    key[1]=Time.parse(key[1])
+    farm_counts[key[0]] = {label:key[0], value: key[1]}
   end
-end
+  farm_counts = farm_counts.sort_by {|_k,v| v[:value].is_a?(String) ? Time.at(0) : v[:value]}.to_h
+  send_event('buzzwords', { items: farm_counts.values })
+#end
+
+
+#buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+#
+#glove={ label: "Yes", value: 555 }
+#send_event('buzzwords', { items: glove.values })
+#
+#
+#require 'pry'; binding.pry
+#
+#send_event('buzzwords', { items: glove.values })
+#
+#send_event('buzzwords', { items: buzzword_counts.values })
+#
+#
+#SCHEDULER.every '10s' do
+#  random_buzzword = buzzwords.sample
+#  buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+#  require 'pry'; binding.pry
+#  
+#  send_event('buzzwords', { items: buzzword_counts.values })
+#end

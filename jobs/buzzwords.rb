cf151c5372b928c6bb919f56760f31c76822cbac
jordanDstout <jordanstout2014@gmail.com>
2015-08-05 10:32:16 -0700

    Added status symbols and color changes to the list widget.

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,18 +1,20 @@
 require 'net/http'
 require 'time'
 
+
 #array used to track which parts of the parsed data are names of farms and not Gnodes
 farm_names=[]
-SCHEDULER.every '5s' do
+#SCHEDULER.every '10s' do
+  DateToday=Time.now
+  DateOneHour=Time.now-(60*60)
   #hash to collect all of the data from parsed JSON
     nodes = {}
     
     #given tokens for an account
 list_of_glocations=['54','212','204']
 
-
   for x in list_of_glocations do
-    #4 lines used to get and parse json
+    #Next 4 lines used to get and parse json
     token_source = "https://secure.gthrive.com/api/client/v2/gnodes.json?glocation_id="+x+"&token=2uaHJ8UzbxxFhzr5w9s5&last_configured_at=2015-07-20T16%3A37%3A30.176Z"
     resp = Net::HTTP.get_response(URI.parse(token_source))
     data = resp.body
@@ -58,11 +60,10 @@ list_of_glocations=['54','212','204']
         currentFarm.insert(0,key)
       end
     end
-    
   end
   #final hash used to compile all of the finalized data that has been sorted
   final1={}  
-  for key in farms
+  for key in farms do
     #make a fresh hash for each array from farms to be sorted by time
     final={}
     key1= key[1]
@@ -73,24 +74,25 @@ list_of_glocations=['54','212','204']
     # able to get it to work), and (more to come as i find them)
     final[key0[0]]={label: "Farm Name", value:key0[1]}
     
-    #for each array within the larger array in farms (two items in each)
-      for x in key1
+      for x in key1 do
+        temp=Time.at(x[1]).to_datetime()
         toFinal=x[0]
-        final[toFinal] = { label: toFinal, value: x[1] }
+        if (DateToday.to_datetime()-temp)>(DateToday.to_datetime()-DateOneHour.to_datetime())
+          status='warning'
+        else
+          status='danger'
+        end
+        
+        final[toFinal] = { label: toFinal, value: x[1], status:status }
       end
       
       #Brad wrote this to sort all of the items by time. The names of the farms
       #were given the oldest possible time so they'd be first in the list
-      one = final.sort_by{|_k,x| x[:value].is_a?(String) ? Time.at(0) : x[:value]}.to_h
+      one = final.sort_by{|_k,xq| xq[:value].is_a?(String) ? Time.at(0) : xq[:value]}.to_h
       
       #merge the sorted hash to the final hash
       final1=one.merge(final1)
   end
   #send the event with the ID farm
   send_event('farm', { items: final1.values })
-  
-end
-
-
-#send_event('farm',{items:[ status:'danger',label:"one", value: 'two']})
-
+#end
--- widgets/list/list.coffee
+++ widgets/list/list.coffee
@@ -4,14 +4,18 @@
 
 class Dashing.List extends Dashing.Widget
 
-  ready: ->
-    if @get('unordered')
-      $(@node).find('ol').remove()
-    else
-      $(@node).find('ul').remove()
+    ready: ->
+        if @get('unordered')
+            $(@node).find('ol').remove()
+        else
+            $(@node).find('ul').remove()
       
-  onData: (data) ->
-        #for x in data
-      #for x in data
-      #  if 
-    $(@node).css('color', '#00ffff')
+    onData: (data) ->
+        console.log data['items']
+        for x in data['items']
+            #if x['status']=='warning'
+             #clear existing "status-*" classes
+            $(@get('node')).attr 'class', (i,c) ->
+                c.replace /\bstatus-\S+/g, ''
+            # add new class
+            $(@get('node')).addClass "status-#{x.status}"
